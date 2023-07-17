f4f32b65d9d44af025f4d3a16a63a6fbb6e32198
tvice <67988191+t-vice@users.noreply.github.com>
2023-07-17 13:02:27 -0400

    Update gpu.sh
    Added correct detection of AMD manufacturer name instead of "Advanced". Should now work as intended for AMD cards.

--- src/gpu.sh
+++ src/gpu.sh
@@ -3,9 +3,19 @@ if lspci | grep -i -q "VGA compatible controller"
 then
     # Extract the manufacturer name from the output of lspci
     manufacturer=$(lspci | grep -i "VGA compatible controller" | awk '{print $5}')
-    
+
+    # vars for AMD
+    AMD="Advanced"
+    AMD2="AMD"
+  
+  # check for "advanced" because AMD is dumb
+  if [ "$manufacturer" = "$AMD" ]
+  then
+    manufacturer="$AMD2"
+  fi
     # Output the manufacturer name
     echo "$manufacturer"
+
 else
     echo "idunno"
 fi
